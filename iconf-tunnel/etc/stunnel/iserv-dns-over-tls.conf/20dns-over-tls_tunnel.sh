b6c56c8354d7a3548c743cee32b3471dfc9a5ea2
Felix Jacobi <Felix@jacobi-hamburg.net>
2019-05-24 13:01:54 +0200

    Split packages into normal one and one providing tls tunnel for standalone use

--- iconf-tunnel/etc/stunnel/iserv-dns-over-tls.conf/20dns-over-tls_tunnel.sh
+++ iconf-tunnel/etc/stunnel/iserv-dns-over-tls.conf/20dns-over-tls_tunnel.sh
@@ -10,13 +10,6 @@ do
   echo "cert = /etc/ssl/certs/iserv.combined"
   echo "key = /etc/ssl/private/iserv.key"
   echo
-  echo "[dns_tls_restricted$COUNT]"
-  echo "accept = $ip:10853"
-  echo "connect = 127.0.0.1:53"
-  echo "local = 127.0.3.$COUNT"
-  echo "cert = /etc/ssl/certs/iserv.combined"
-  echo "key = /etc/ssl/private/iserv.key"
-  echo
   let COUNT++
 done < <(netquery -p ip)
 
@@ -27,13 +20,6 @@ then
     echo "[dns_tls$COUNT]"
     echo "accept = $ip:853"
     echo "connect = $ip:53"
-    echo "cert = /etc/ssl/certs/iserv.combined"
-    echo "key = /etc/ssl/private/iserv.key"
-    echo
-    echo "[dns_tls_restricted$COUNT]"
-    echo "accept = $ip:10853"
-    echo "connect = 127.0.0.1:53"
-    echo "local = 127.0.3.$COUNT"
     echo "cert = /etc/ssl/certs/iserv.combined"
     echo "key = /etc/ssl/private/iserv.key"
     echo
---
+++ iconf/etc/stunnel/iserv-dns-over-tls.conf/25dns-over-tls_tunnel_restricted.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+COUNT=1
+
+while read ip
+do
+  echo "[dns_tls_restricted$COUNT]"
+  echo "accept = $ip:10853"
+  echo "connect = 127.0.0.1:53"
+  echo "local = 127.0.3.$COUNT"
+  echo "cert = /etc/ssl/certs/iserv.combined"
+  echo "key = /etc/ssl/private/iserv.key"
+  echo
+  let COUNT++
+done < <(netquery -p ip)
+
+if [ -x "$(which netquery6)" ]
+then
+  while read ip
+  do
+    echo "[dns_tls_restricted$COUNT]"
+    echo "accept = $ip:10853"
+    echo "connect = 127.0.0.1:53"
+    echo "local = 127.0.3.$COUNT"
+    echo "cert = /etc/ssl/certs/iserv.combined"
+    echo "key = /etc/ssl/private/iserv.key"
+    echo
+    let COUNT++
+done < <(netquery6 -gul ip)
+
+fi
