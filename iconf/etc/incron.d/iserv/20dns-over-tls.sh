c8112dc01a100cf4fad85980df3be612c4471268
Felix Jacobi <felix.jacobi@iserv.eu>
2019-09-05 17:44:34 +0200

    Changed incrond job for ip list update

--- iconf/etc/incron.d/iserv/20dns-over-tls.sh
+++ iconf/etc/incron.d/iserv/20dns-over-tls.sh
@@ -7,7 +7,6 @@ then
   for iplist in /var/lib/iserv/portal/bind.restricted-ips.d/*
   do
     echo "$iplist IN_CLOSE_WRITE /usr/lib/iserv/sleep_exec_once 3 dns-over-tls-update-iplist /usr/lib/iserv/dns-over-tls_update-ipset"
-    echo "$iplist IN_OPEN /usr/lib/iserv/sleep_exec_once 3 dns-over-tls-update-iplist /usr/lib/iserv/dns-over-tls_update-ipset"
   done
   echo
 fi
